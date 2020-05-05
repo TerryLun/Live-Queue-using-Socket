2ad947716e4811c7d88d8caced4cba81be3b2146
TerryLun <tianweilun@yahoo.com>
2020-05-04 23:40:00 -0700

    format client chat message

--- server.js
+++ server.js
@@ -11,25 +11,31 @@ const app = express();
 const PORT = 3000 || process.env.PORT;
 const server = http.createServer(app);
 const io = socketio(server);
+const botName = 'ChatBot';
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+
 io.on('connection', (socket) => {
    //emit welcome message to single client
-   socket.emit('message', 'Welcome to ChatCord!');
+   socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
 
    //broadcast when a user connects
    //broadcast to every client except the one thats conencting
-   socket.broadcast.emit('message', 'A user has joined the chat');
+   socket.broadcast.emit(
+      'message',
+      formatMessage(botName, 'A user has joined the chat')
+   );
 
    socket.on('disconnect', () => {
       //broadcast to every client
-      io.emit('message', 'A user has left the chat.');
+      io.emit('message', formatMessage(botName, 'A user has left the chat.'));
    });
 
    // listen for emitted chat message
    socket.on('chatMessage', (msg) => {
       // console.log(msg);
-      io.emit('message', msg);
+      io.emit('message', formatMessage('USER', msg));
    });
 });
 

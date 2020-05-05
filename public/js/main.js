71af224da4f92e7501aa6b038eaf22ac6dcff42c
TerryLun <tianweilun@yahoo.com>
2020-05-04 23:19:06 -0700

    output reveived message to chat board

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,10 @@
 const socket = io();
 const chatForm = document.getElementById('chat-form');
 
+// message from server
 socket.on('message', (message) => {
-   console.log(message);
+   // console.log(message);
+   outputMessage(message);
 });
 
 // message submit
@@ -16,3 +18,16 @@ chatForm.addEventListener('submit', (e) => {
    // console.log(msg);
    socket.emit('chatMessage', msg);
 });
+
+// output reveived message to chat board
+function outputMessage(message) {
+   const div = document.createElement('div');
+   div.classList.add('message');
+   div.innerHTML = `
+   <p class="meta">Brad <span>9:12pm</span></p>
+   <p class="text">
+      ${message}
+   </p>
+   `;
+   document.querySelector('.chat-messages').appendChild(div);
+}

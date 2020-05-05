cc136ce23377ab8d26c0bda53b7e47daf0c979ab
TerryLun <tianweilun@yahoo.com>
2020-05-04 21:35:33 -0700

    emit message to server

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,18 @@
 const socket = io();
+const chatForm = document.getElementById('chat-form');
 
 socket.on('message', (message) => {
    console.log(message);
+});
+
+// message submit
+chatForm.addEventListener('submit', (e) => {
+   e.preventDefault();
+
+   // get submitted message
+   const msg = e.target.elements.msg.value;
+
+   // emit chat message to server
+   // console.log(msg);
+   socket.emit('chatMessage', msg);
 });
